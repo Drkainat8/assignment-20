174d40c486ab0d5a536d3c02337ab8225c95a456
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-06-15 09:37:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// assignment 20
+//  list of cities store in array
+var cityname = ["karachi", "lahor", "hyderabad", "multan", "faislabad", "islamabad"];
+// print city name iin list
+console.log("list of city", cityname.forEach(function (list) {
+    console.log(list);
+}));
---
+++ main.ts
@@ -0,0 +1,7 @@
+// assignment 20
+//  list of cities store in array
+let cityname : string[] = [`karachi`,`lahor`,`hyderabad`,`multan`,`faislabad`,`islamabad`]
+// print city name iin list
+console.log(`list of city`,cityname.forEach(list => {
+    console.log(list)
+}))
